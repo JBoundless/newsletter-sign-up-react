8035bfbb80600ff082f7427015487baa68ba0151
Jordan Pagkalinawan <39219944+JBoundless@users.noreply.github.com>
2023-05-31 11:26:19 -0700

    Update Success.js

--- src/Success.js
+++ src/Success.js
@@ -5,7 +5,7 @@ import iconSuccess from './images/icon-success.svg';
 export default function Success({email, setSuccess}) {
   return (
     <div className="success">
-      <img src={...iconSuccess} />
+      <img src='https://raw.githubusercontent.com/JBoundless/newsletter-sign-up-react/main/src/images/icon-success.svg' />
       <h1>Thanks for subscribing!</h1>
       <p>
         A confirmation email has been sent to <strong>{email}</strong>. Please open it and click the button inside to confirm your subscription.
